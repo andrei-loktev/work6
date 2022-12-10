24095702710aa7eec9296cb310c72bc547d67919
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2022-12-10 15:20:19 +0300

    задание 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        int clientOS = 2;
+        //задание 1
+        System.out.println("задание 1");
+        int clientOS = 1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        else if (clientOS == 1) {
+        } else if (clientOS == 1) {
            System.out.println("Установите версию приложения для Android по ссылке");
            } else {
            System.out.println("обратиться за помощью");
        }
+        //задание 2
+        System.out.println("задание 2");
+        int clientDeviceYear = 2013;
+        if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("Установите обычную версию приложения для iOS по ссылке");
+        }
+        if (clientDeviceYear < 2015 && clientOS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
+        }
     }
 }
